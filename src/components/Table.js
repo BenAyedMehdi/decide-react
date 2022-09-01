86ed3eb13140eef6d18763e54e0b54c13144ade4
Ben Ayed Mehdi <mehdibenayed97@gmail.com>
2022-09-01 02:11:05 +0200

    table sorted with colors

--- src/components/Table.js
+++ src/components/Table.js
@@ -10,14 +10,24 @@ import Paper from '@mui/material/Paper';
 export default function BasicTable({pros, cons}) {
 
     const createElements = (pros, cons) =>{
+      pros.sort((a,b) =>{
+        return b.value- a.value
+      })
+      cons.sort((a,b) =>{
+        return b.value- a.value
+      })
         const maxLength = pros.length > cons.length ? pros.length : cons.length
         let arr = []
         for(let i = 0 ; i< maxLength ; i++){
             let row = []
-            const prosItem = pros[i]? pros[i].reason + ' (' + pros[i].value + ')' : ''
-            const consItem = cons[i]? cons[i].reason + ' (' + cons[i].value+ ')': ''
+            const prosValue = pros[i]? pros[i].value : ''
+            const prosItem = pros[i]? pros[i].reason : ''
+            const consItem = cons[i]? cons[i].reason : ''
+            const consValue = cons[i]? cons[i].value : ''
+            row.push(prosValue)
             row.push(prosItem)
             row.push(consItem)
+            row.push(consValue)
             arr.push(row)
         }
         return arr
@@ -28,20 +38,31 @@ export default function BasicTable({pros, cons}) {
       <Table sx={{ minWidth: 300 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Pros</TableCell>
-            <TableCell align="right">Cons</TableCell>
+            <TableCell size='small'>Value</TableCell>
+            <TableCell style={{backgroundColor:'green', color: 'white',}}>Pros</TableCell>
+            <TableCell style={{backgroundColor:'red', color: 'white',}} align="right">Cons</TableCell>
+            <TableCell size='small' align="right">Value</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {createElements(pros,cons).map((row, index) => (
             <TableRow
+              hover
               key={index}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell component="th" scope="row">
-                {row[0]}
-              </TableCell>
-              <TableCell align="right">{row[1]}</TableCell>
+              <TableCell component="th" scope="row"> {row[0]}</TableCell>
+
+              { row[0]==5 &&  <TableCell style={{backgroundColor:"#FF8C00"}}  component="th" scope="row">{row[1]}</TableCell>}
+              { row[0]==3 && <TableCell style={{backgroundColor:"#FFDAB9"}}  component="th" scope="row">{row[1]}</TableCell>}
+              { row[0]==1 &&  <TableCell component="th" scope="row">{row[1]}</TableCell>}
+              
+
+              { row[3]==5 &&  <TableCell style={{backgroundColor:"#FF8C00"}}  component="th" scope="row">{row[2]}</TableCell>}
+              { row[3]==3 && <TableCell style={{backgroundColor:"#FFDAB9"}}  component="th" scope="row">{row[2]}</TableCell>}
+              { row[3]==1 &&  <TableCell component="th" scope="row">{row[2]}</TableCell>}
+              
+              <TableCell align="right">{row[3]}</TableCell>
              
             </TableRow>
           ))}
